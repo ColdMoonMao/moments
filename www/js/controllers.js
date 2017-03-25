7ab3b8a14b57e816ad48cf80e9bc4bee6f402dc8
MAO\mao <364562354@qq.com>
2017-03-25 23:26:21 +0800

    修复点赞bug

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,29 @@
 angular.module('starter.controllers', [])
 
 .controller('MommentCtrl', function($scope,MonmmentService) {
-	var favourClick=false;
 	//scope变量定义
-	$scope.hasNew = true;//是否有新消息
+	$scope.hasNew = [];//是否有新消息
 	$scope.userName='张小六';//登录的用户
 	$scope.commentNum=3;
 	$scope.commentClick=false;
 	$scope.viewAllTextHead='查看全部';
 	$scope.viewAllTextEnd='条评论';
+	$scope.hasFavour=false;
 	//从MonmmentService中获取数据;
 	$scope.data=MonmmentService.all();
 	//点赞函数
 	$scope.onFavorClick=function () {
-		// console.log(this.value.favourIco);
-		favourClick=!favourClick;
-		if (favourClick) {
+		console.log(this.value.favourIco);
+	$scope.hasFavour=!$scope.hasFavour;
+		if (this.value.favourIco=='ion-ios-heart-outline') {
 			this.value.favour++;
 			this.value.favourIco='ion-ios-heart favourIco';
-			$scope.hasNew = true;
-		} else {
+			console.log('test');
+			$scope.hasNew.push(1);
+		} else if(this.value.favourIco=='ion-ios-heart favourIco'){
 			this.value.favour--;
-			this.value.favourIco='ion-ios-heart-outline grey';
-			$scope.hasNew = false;
+			this.value.favourIco='ion-ios-heart-outline';
+			$scope.hasNew.pop();
 		}
 	}
 	//点击展开评论列表函数
